5d46df4e109d426b452d1c488d099d938834b1d9
Jay Lockridge <jklockri@pnw.edu>
2017-03-07 19:24:18 -0600

    Fixed spelling of integer

--- db/migrate/20170308012342_create_products.rb
+++ db/migrate/20170308012342_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.integar :price
+      t.integer :price
       t.string :image
       t.string :description
 
